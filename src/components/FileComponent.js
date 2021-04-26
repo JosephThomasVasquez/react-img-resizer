5ae15e85f85c4ed90a42c0bcefc04f7ef66e9ba9
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-26 13:26:57 -0700

    Added handleChange function to update state with files array

--- src/components/FileComponent.js
+++ src/components/FileComponent.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FileComponent = () => {
+  const [files, setFiles] = useState([]);
+
+  const handleChange = (e) => {
+    console.log("files event >", e.target.files);
+
+    setFiles(files.push(e.target.files));
+
+    console.log("files >", files);
+  };
+
   return (
     <div className="image-form-container">
       <h3>Choose a file to edit.</h3>
       <form className="image-form-">
         <label className="file-selector-label">
           Browse
-          <input type="file" id="file-selector-default" multiple />
+          <input
+            type="file"
+            id="file-selector-default"
+            onChange={handleChange}
+            multiple
+          />
         </label>
         <input type="text" id="file-selector-name" placeholder="Filename" />
       </form>
+      {files && <div>{files.length}</div>}
     </div>
   );
 };
