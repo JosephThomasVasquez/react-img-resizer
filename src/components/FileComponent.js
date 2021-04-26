273d77953d4379f372bd12d6b9ec862fa4ba8eeb
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-26 13:46:21 -0700

    fiels state returns list items

--- src/components/FileComponent.js
+++ src/components/FileComponent.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 
 const FileComponent = () => {
+    
   const [files, setFiles] = useState([]);
 
   const handleChange = (e) => {
     console.log("files event >", e.target.files);
+    console.log("files >", files.length);
 
-    setFiles(files.push(e.target.files));
+    setFiles([...e.target.files]);
 
     console.log("files >", files);
+    console.log(files[0])
   };
 
   return (
     <div className="image-form-container">
       <h3>Choose a file to edit.</h3>
-      <form className="image-form-">
+      <form className="image-form">
         <label className="file-selector-label">
           Browse
           <input
@@ -26,7 +29,10 @@ const FileComponent = () => {
         </label>
         <input type="text" id="file-selector-name" placeholder="Filename" />
       </form>
-      {files && <div>{files.length}</div>}
+      <h3>{files && `${files.length} files`}</h3>
+      <ul>{files && files.map((file) => (
+          <li key={file.name}>{file.name}</li>
+      ))}</ul>
     </div>
   );
 };
