8016c401e5f27c860a0e5583ec64350e0c16d391
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-26 13:02:17 -0700

    Added FileComponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import NavBarComponent from './components/NavBarComponent';
+import FileComponent from './components/FileComponent';
 
 function App() {
   return (
     <div className="App">
+      
       <NavBarComponent />
+
+      
       <h1 className="title">Image Resizer</h1>
+      
+      <FileComponent />
+    
     </div>
   );
 }
