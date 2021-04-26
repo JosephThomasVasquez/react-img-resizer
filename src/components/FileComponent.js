5b78e7f4a236c7334cc0e462f5387d2b4715b5a7
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-26 13:05:46 -0700

    Added input form html elements

--- src/components/FileComponent.js
+++ src/components/FileComponent.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const FileComponent = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="image-form-container">
+      <h3>Choose a file to edit.</h3>
+      <form className="image-form-">
+        <label className="file-selector-label">
+          Browse
+          <input type="file" id="file-selector-default" multiple />
+        </label>
+        <input type="text" id="file-selector-name" placeholder="Filename" />
+      </form>
+    </div>
+  );
+};
 
-export default FileComponent
+export default FileComponent;
