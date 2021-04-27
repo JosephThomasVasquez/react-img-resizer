2f101274b21473f75deae364072b81f4f56fe4a9
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-27 09:13:50 -0700

    Added updateCanvas function and FileReader setup to convert file data to usable img DOM element and render to canvas

--- src/components/CanvasComponent.js
+++ src/components/CanvasComponent.js
@@ -1,23 +1,42 @@
 import React, { useState, useEffect, useRef } from "react";
 
 const CanvasComponent = ({ file }) => {
-  const [imageFile, setImageFile] = useState();
+  const [imageFile, setImageFile] = useState({});
 
   //   console.log(file)
   const canvasRef = useRef(null);
+
+  const updateCanvas = (ctx, img) => {
+    ctx.drawImage(img, 0, 0);
+  };
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
 
     if (file) {
-      console.log(file);
+      let reader = new FileReader();
+      console.log("reader", reader);
+
+      reader.onload = (e) => {
+        let imgFile = new Image();
+        imgFile.onload = () => {
+          updateCanvas(context, imgFile);
+        };
+        imgFile.src = e.target.result;
+      };
+
+      reader.readAsDataURL(file);
+
+      setImageFile({ ...file });
+      console.log(imageFile);
+      //   updateCanvas(context);
     }
   }, [file]);
 
   return (
     <div>
-      <img className="img-file" src={file && file.url} alt="" />
+      <img className="img-file" src={file && file.url} alt="file.name" />
       <canvas ref={canvasRef} className="canvas-preview"></canvas>
     </div>
   );
