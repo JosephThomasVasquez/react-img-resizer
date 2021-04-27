7c861079eba5d9efd2c88c85110699340bcc5894
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-27 11:17:39 -0700

    Added scaleImageHandler which updates state when range slider is changed

--- src/components/CanvasComponent.js
+++ src/components/CanvasComponent.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 
 const CanvasComponent = ({ file }) => {
   const [imageFile, setImageFile] = useState({});
+  const [imageScale, setImageScale] = useState(100);
 
   //   console.log(file)
   const canvasRef = useRef(null);
@@ -14,27 +15,32 @@ const CanvasComponent = ({ file }) => {
     // console.log('aspectRatio >', aspectRatio);
 
     // Set a scale factor and resize the iamge
-    let scale = 0.035;
-    const newWidth = (img.height * aspectRatio) * scale;
+    let scale = imageScale * 0.001;
+    console.log(scale)
+    const newWidth = img.height * aspectRatio * scale;
     const newHeight = (img.width / aspectRatio) * scale;
 
     // Set the center of the canvas based on image dimensions after scaling
-    const centerWidth = (width / 2) - (newWidth / 2);
-    const centerHeight = (height / 2) - (newHeight / 2);
+    const centerWidth = width / 2 - newWidth / 2;
+    const centerHeight = height / 2 - newHeight / 2;
 
     ctx.drawImage(img, centerWidth, centerHeight, newWidth, newHeight);
+  };
+
+  const scaleImageHandler = (e) => {
+    setImageScale(e.target.value);
   };
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
 
-    const {width} = context.canvas;
-    const {height} = context.canvas;
+    const { width } = context.canvas;
+    const { height } = context.canvas;
 
     if (file) {
       let reader = new FileReader();
-      console.log("reader", reader);
+    //   console.log("reader", reader);
 
       reader.onload = (e) => {
         let imgFile = new Image();
@@ -50,13 +56,25 @@ const CanvasComponent = ({ file }) => {
       console.log(imageFile);
       //   updateCanvas(context);
     }
-  }, [file]);
+  }, [file, imageScale]);
 
   return (
     <div>
-      <img className="img-file" src={file && file.url} alt={file && file.name} />
+      <img
+        className="img-file"
+        src={file && file.url}
+        alt={file && file.name}
+      />
       <br></br>
-      <input type="range" name="scaleImage" id="scale-img" className="slider-scale"/>
+      <input
+        type="range"
+        name="scaleImage"
+        id="scale-img"
+        className="slider-scale"
+        min="0"
+        max="100"
+        onChange={scaleImageHandler}
+      />
       <canvas ref={canvasRef} className="canvas-preview"></canvas>
     </div>
   );
